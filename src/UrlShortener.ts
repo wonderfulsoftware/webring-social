da95c73b90efc7147a311c85404ce0f8f59efccf
dtinth on MBP M1 <dtinth@spacet.me>
2024-01-18 01:11:42 +0700

    fix the new url shortener implementation

--- src/UrlShortener.ts
+++ src/UrlShortener.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 export async function createShortUrl(url: string) {
   const response = await redaxios.post(
     `https://link.webring.in.th/links`,
-    { link: url },
+    { url },
     {
       headers: {
         'x-api-key': env.LINK_API_KEY,
@@ -12,5 +12,5 @@ export async function createShortUrl(url: string) {
     },
   )
   const data = response.data
-  return data.shortLink
+  return data.url
 }
